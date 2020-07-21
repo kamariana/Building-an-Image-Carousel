042eee50be9d1976371ac33ea7f7514848133e44
thurab40 <thurab114@gmail.com>
2020-07-21 07:05:04 +0000

    add the image data structure array

---
+++ app.js
@@ -0,0 +1,15 @@
+const images = [
+    './sunset-castle-of-Marvao.jpg',
+    './taiwan-island.jfif'
+];
+
+let currentIndex = 0; // the current of the image
+
+
+
+// 
+function incrementIndex() {
+    currentIndex += 1;
+}
+
+
