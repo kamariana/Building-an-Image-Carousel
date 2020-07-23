6d16bc1c5a607282164048718d97102d856b8556
thurab40 <thurab114@gmail.com>
2020-07-23 12:19:38 +0000

    add more helper functions and click events for next and prev images

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 const images = [
     './sunset-castle-of-Marvao.jpg',
-    './taiwan-island.jfif'
+    './taiwan-island.jfif',
+    './mussender-temple.jfif'
 ];
 
 let currentIndex = 0; // the current of the image
+let img = document.querySelector('.js-img');
+let next = document.querySelector('.js-next');
+let prev = document.querySelector('.js-prev');
 
+console.log(
+    images[0]
+);
+// Helper functions
+const incrementIndex = () => {
+    currentIndex = currentIndex + 1;
+    if (currentIndex > images.length - 1) {
+        currentIndex = 0;
+    }
 
-
-// 
-function incrementIndex() {
-    currentIndex += 1;
+    return currentIndex;
 }
+
+const decrementIndex = () => {
+    currentIndex = currentIndex - 1;
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    }
+
+    return currentIndex;
+}
+
+
+// Event Listeners
+next.addEventListener('click', (event) => {
+    img.setAttribute('src', images[incrementIndex(currentIndex)])
+});
+
+prev.addEventListener('click', (event) => {
+    img.setAttribute('src', images[decrementIndex(currentIndex)])
+});
 
 
